d056f8b12c207311bb02704cd162aa0a9967dd28
sajjathossain <sajjathossain.official@gmail.com>
2024-08-04 21:45:45 +0600

    chore(app): update styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { Card } from './components';
 
 export function Main() {
   return (
-    <div className="w-screen h-screen grid place-items-center bg-slate-200">
+    <div className="w-full min-h-screen grid place-items-center bg-slate-200">
       <Card />
     </div>
   );
