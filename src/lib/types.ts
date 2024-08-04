0a01c4ad5ea37d7345eed2d34da91cfbddcfeda5
sajjathossain <sajjathossain.official@gmail.com>
2024-08-04 15:23:51 +0600

    feat(lib/type): move types inside the types file

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,1 +1,19 @@
+export type TCardButton = {
+  onClick: (params?: { isHorizontal?: boolean }) => void;
+  isHorizontal?: boolean;
+};
 
+export type TCardProps =
+  | {
+    children: true;
+    isHorizontal?: boolean;
+  }
+  | {
+    children?: false;
+    isHorizontal?: boolean;
+  };
+
+export type TMergeButton = {
+  onClick: () => void;
+  isHorizontal?: boolean;
+};
